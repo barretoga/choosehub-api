ccc78682b86d0ad5c6bd82e19c096fda1407d85c
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-09-04 19:11:09 -0300

    feat: create book service

---
+++ src/main/java/com/barretoga/choosehub/book/BookService.java
@@ -0,0 +1,30 @@
+package com.barretoga.choosehub.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public List<Book> listBooks(){
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void removeBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
